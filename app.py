5859e981e886a451a2c3a49be71d81587f30b6f4
EdenDGabay <eden.gabay1996@gmail.com>
2024-03-05 18:07:19 +0200

    Base, directories and basic HTML pages work with their Python. Missing CSS, JS and DB connections.f

--- app.py
+++ app.py
@@ -14,8 +14,21 @@ app.register_blueprint(Categories, url_prefix='/categories')
 
 @app.route('/')
 def home():
-    # Redirect to the homepage Blueprints
-    return redirect('/homepage')
+    categories = [
+        {'name': 'Mystery', 'filename': 'MysteryCategory.jpg'},
+        {'name': 'Fantasy', 'filename': 'FantasyCategory.jpg'},
+        {'name': 'Biography', 'filename': 'BiographyCategory.jpg'},
+        {'name': 'Horror', 'filename': 'HorrorCategory.jpg'},
+        {'name': 'Romance', 'filename': 'RomanceCategory.jpeg'},
+        {'name': 'Comedy', 'filename': 'ComedyCategory.jpg'},
+        {'name': 'Adventure', 'filename': 'AdventureCategory.jpg'},
+        {'name': 'Science Fiction', 'filename': 'ScienceFictionCategory.jpg'},
+        {'name': 'History', 'filename': 'HistoryCategory.png'},
+    ]
+    return render_template('HomePage.html', categories=categories)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
